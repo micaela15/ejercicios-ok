03778a9955222a9cc68957c358056a372c5371cb
micaela15 <40809059+micaela15@users.noreply.github.com>
2018-07-10 12:26:57 -0300

    Add files via upload

---
+++ jsInstruccionIF (1).js
@@ -0,0 +1,14 @@
+function mostrar()
+{
+//tomo la edad  
+   var edad;
+   If (edad=="15");
+   edad = document.getElementById("edad").value;
+   edad = parseInt(edad);
+   
+
+
+
+
+}//FIN DE LA FUNCIÓN
+alert("niña bonita");
